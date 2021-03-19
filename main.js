90bb12ada9f785737249f198a68ff37980d3d010
edolezalova <edolezalova@gmail.com>
2021-03-19 13:16:16 +0100

    moje upravy

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // tady je místo pro náš program
+
+let nadpis = document.querySelector("h1");
+
+nadpis.classList.toggle("zluty");
+
+function zmenaNadpisu (nadpis) {
+    nadpis.innerHTML="Nový nadpis";
+}
+
+let odstavec = document.querySelector("p")[0];
+
+function ztucni(odstavec) {
+    console.log ("baf");
+    odstavec.target.style.fontWeight="bold";
+}
+
+function zcervenej (nadpis) {
+    nadpis.target.style.color="red";
+}
